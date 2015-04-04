5e07485eb2ba222bbab2c735a01b401ccfb3ab8b
lc <lc@qq.com>
2015-04-04 15:22:06 +0800

    Binary Tree Right Side View

---
+++ 199.cpp
@@ -0,0 +1,49 @@
+/************************************************************************/
+/* 二叉树层次遍历方法                                                   */
+/************************************************************************/
+ 
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode *root) {
+		queue<TreeNode *> myQue;
+		vector<int> res;		
+		res.clear();
+
+		if (NULL == root)
+		{
+			return res;
+		}
+		
+		myQue.push(root);
+		myQue.push(NULL);
+		TreeNode *temp1,*temp2;
+
+		while (true)
+		{
+			temp1 = myQue.front();
+			myQue.pop();
+			temp2 = myQue.front();
+			//myQue.pop();
+			if(NULL == temp1 && NULL == temp2)
+				break;
+			if(NULL == temp1)
+			{
+				myQue.push(NULL);//为下一层添加标记
+				continue;
+
+			}
+			if (NULL == temp2)//提取下一个指针，判断是否是标记NULL
+			{
+				res.push_back(temp1->val);//保存最右边的值
+			}			
+			
+			if(NULL != temp1->left)
+				myQue.push(temp1->left);
+			if (NULL != temp1->right)
+			{
+				myQue.push(temp1->right);
+			}
+		}
+		return res;        
+    }
+};
